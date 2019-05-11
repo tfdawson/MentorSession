971fddd99bc60397667c9b75884435d30ed80491
tyler dawson <tyler@tylers-imac.attlocal.net>
2019-05-10 21:50:05 -0400

    JS practice

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log('Hello World!');
+let x=5
+const y='Hello World!';
+var d='How is it going?';
+
+for (let i = 0; i < 5; i++){
+    console.log(i);  
+}
+
+
+let i=0
+while (i<5){
+    console.log(i) 
+    i++
+}
+
+let myList="apples, oranges, bananas, grapes"{
+    console.log(myList)
+}
+
+
+function myFunction(a,b) {
+    return a + b
+}
+
+function returnAString() {
+    return("Tyler Dawson")
+}
+
+let myName=returnAString() 
+
+console.log(returnAString())
+
+
+
+
